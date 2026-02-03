860a377d336738288686a0d0c4d909c5c04d1523
RedKrane86 <redkrane86@gmail.com>
2026-02-03 16:35:08 +0500

    Reformating

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-age = int(input('Введите возраст:\n'))
+def check_age(age):
+    if age >= 18:
+        return True
+    return False
 
-is_citizen = input('Вы гражданин этой страны? (да/нет):\n')
-if is_citizen.lower() == 'да':
-    is_citizen = True
-else:
-    is_citizen = False
+def is_citizen_check(answer):
+    if answer.lower() == 'да':
+        return True
+    return False
 
-is_disqualified = input('Имеется ли дисквалификация/запрет на участие в голосовании? (да/нет):\n')
-if is_disqualified.lower() == 'да':
-    is_disqualified = True
-else:
-    is_disqualified = False
+def is_disqualified_check(answer):
+    if answer.lower() == 'да':
+        return True
+    return False
 
-if age >= 18 and is_citizen and not is_disqualified:
-    print('Вы имеете право голосовать')
-else:
-    print('Вы не имеете права голосовать')
+def main_check(is_age, is_citizen, is_disqualified):
+    if is_age and is_citizen and not is_disqualified:
+        return 'Вы имеете право голосовать'
+    else:
+        return 'Вы не имеете права голосовать'
+
+
+user_age = int(input('Введите возраст:\n'))
+user_is_citizen = input('Вы гражданин этой страны? (да/нет):\n')
+user_is_disqualified = input('Имеется ли дисквалификация/запрет на участие в голосовании? (да/нет):\n')
+
+print(main_check(
+    check_age(user_age),
+    is_citizen_check(user_is_citizen),
+    is_disqualified_check(user_is_disqualified))
+)
